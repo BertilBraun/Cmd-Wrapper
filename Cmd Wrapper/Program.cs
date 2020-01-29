6c23023d5afedc79ab54520256ae95f7f9e74b11
OrtwIn <braunbertil123@outlook.de>
2020-01-29 13:50:15 +0100

    Add files via upload

--- Cmd Wrapper/Program.cs
+++ Cmd Wrapper/Program.cs
@@ -9,38 +9,47 @@ namespace Cmd_Wrapper
         static void Main(string[] args)
         {
             string WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\";
+            Process p = new Process();
+
+            p.StartInfo.FileName = "cmd.exe";
+
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+
+            p.OutputDataReceived += (a, b) => Console.WriteLine(b.Data);
+            p.ErrorDataReceived += (a, b) => Console.WriteLine(b.Data);
 
             while (true)
             {
-                using (Process p = new Process())
+                Console.Write(WorkingDirectory + "\b>");
+                var cmd = Console.ReadLine();
+
+                p.StartInfo.Arguments = @"/c " + cmd;
+                p.StartInfo.WorkingDirectory = WorkingDirectory;
+                
+                if (cmd.StartsWith("cd"))
+                    WorkingDirectory = Path.GetFullPath(Path.Combine(WorkingDirectory, cmd.Remove(0, 2).Trim().Trim('\"', '\'') + @"\"));
+
+                try
                 {
-                    Console.Write(WorkingDirectory + "\b>");
-                    var cmd = Console.ReadLine();
-
-                    p.StartInfo.FileName = "cmd.exe";
-                    p.StartInfo.Arguments = @"/c " + cmd;
-                    p.StartInfo.WorkingDirectory = WorkingDirectory;
-
-                    p.StartInfo.CreateNoWindow = true;
-                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    
-                    p.StartInfo.RedirectStandardError = true;
-                    p.StartInfo.RedirectStandardOutput = true;
-                    p.StartInfo.RedirectStandardInput = true;
-                    
-                    p.OutputDataReceived += (a, b) => Console.WriteLine(b.Data);
-                    p.ErrorDataReceived += (a, b) => Console.WriteLine(b.Data);
-                    
                     p.Start();
-                    
-                    p.BeginErrorReadLine();
-                    p.BeginOutputReadLine();
-
-                    p.WaitForExit();
-
-                    if (cmd.StartsWith("cd"))
-                        WorkingDirectory = Path.GetFullPath(Path.Combine(WorkingDirectory, cmd.Remove(0, 2).Trim().Trim('\"', '\'') + @"\"));
                 }
+                catch (Exception)
+                {
+                    WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\";
+                }
+
+                p.BeginErrorReadLine();
+                p.BeginOutputReadLine();
+
+                p.WaitForExit();
+
+                p.CancelErrorRead();
+                p.CancelOutputRead();
             }
         }
     }
